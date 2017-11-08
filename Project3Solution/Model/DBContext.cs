23aa897aa7c265c013cb5d5af892ffb5537121d5
Mihail Zdravkov Zdravkov <1061991@ucn.dk>
2017-11-08 12:50:07 +0100

    Integrated EntityFramework, connected to Kraka

---
+++ Project3Solution/Model/DBContext.cs
@@ -0,0 +1,34 @@
+namespace Model
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class DBContext : DbContext
+    {
+        // Your context has been configured to use a 'DBContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'Model.DBContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'DBContext' 
+        // connection string in the application configuration file.
+        public DBContext()
+            : base("name=DBContext")
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
