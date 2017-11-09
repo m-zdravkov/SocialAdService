2bfb9275bab8e01c886be0475f86cab8b973ad88
rootyTooty <rootyTooty@fartsan>
2017-11-09 12:42:57 +0100

    Added Post Validator class, and PostValidatorTest class.

---
+++ Project3Solution/Model/PostValidator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Model
+{
+    public class PostValidator
+    {
+        private static Regex _contentRegex = new Regex(@"[a-zA-Z]{4,128}");
+        //TO DO: Test if the Regex for URLS works.
+        private static Regex _imageSourceRegex = new Regex(@"^http(s)?://([\w-]+.)+[\w-]+(/[\w- ./?%&=])?$");
+
+        public Post postObject { get; set; }
+        public PostValidator (Post post)
+        {
+            postObject = post;
+        }
+
+        public bool ValidatePost ()
+        {
+            bool result = true;
+
+            result = result && validateContent(postObject.Content);
+            result = result && validateImageSource(postObject.ImageSource);
+
+            return result;
+        }
+
+        public static bool validateContent (string content)
+        {
+            return _contentRegex.Match(content).Success;
+        }
+
+        public static bool validateImageSource (string imageSource)
+        {
+            return _imageSourceRegex.Match(imageSource).Success;
+        }
+    }
+}
---
+++ Project3Solution/ModelTests/PostValidatorTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+
+namespace ModelTests
+{
+    [TestClass]
+    public class PostValidatorTest
+    {
+        [TestMethod]
+        //Test minimum character limit
+        public void TestContent1()
+        {
+            Post postObject = new Post
+            {
+                Content = "1234",
+                ImageSource = "NaN"
+                
+            };
+
+            PostValidator validator1 = new PostValidator(postObject);
+
+            Assert.IsFalse(validator1.ValidatePost());
+
+        }
+
+
+        [TestMethod]   
+        //Test maximum character limit
+        public void TestContent2()
+        {
+            Post postObject = new Post
+            {
+                Content = "Lorem ipsum dolor sit amet, consetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque pen",
+                ImageSource = "NaN"
+                
+            };
+
+            PostValidator validator1 = new PostValidator(postObject);
+
+            Assert.IsFalse(validator1.ValidatePost());
+        }
+        [TestMethod]
+        //Test URL link
+        //NOTE: Changed: Assert.IsFalse TO Assert.IsTrue
+        public void TestImageSource1()
+        {
+            Post postObject = new Post
+            {
+                Content = "NaNi",
+                ImageSource = "http://images.com/myimage.jpg"
+            };
+
+            PostValidator validator1 = new PostValidator(postObject);
+
+            Assert.IsTrue(validator1.ValidatePost());
+        }
+    }
+}
