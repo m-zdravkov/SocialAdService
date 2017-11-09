8aad8c24b01cbed4829a2ddc9fefa9d1fd4590c3
rootyTooty <rootyTooty@fartsan>
2017-11-09 13:32:05 +0100

    Added the Ad class.

---
+++ Project3Solution/Model/Ad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Model
+{
+    class Ad : Post
+    {
+        public Price Price = new Price();
+
+        public ISet<String> Categories;
+
+        public DateTime ExpDate { get; set; }
+
+        public int Views { get; set; }
+
+
+    }
+}
---
+++ Project3Solution/Model/Price.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Model
+{
+    public enum PriceType { Range, Free, Unspecified, Bid }
+    class Price
+    {
+        public int Low { get; private set; }
+        public int High { get; private set; }
+        public PriceType Type { get; private set; }
+    }
+}
