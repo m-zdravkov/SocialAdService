63462011e65e98ad08a0e3aeeb251498c7ff1007
rootyTooty <rootyTooty@fartsan>
2017-11-10 11:03:01 +0100

    Added the Comment class, and the Comment Validator class.

---
+++ Project3Solution/Model/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Comment : Post
+    {
+        public Post repliedTo { get; set; }
+
+        public int level { get; set; }
+    }
+}
---
+++ Project3Solution/Model/CommentValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Model
+{
+    public static class CommentValidator
+    {
+        public static bool validateComment (this Comment comment)
+        {
+            bool result = true;
+
+            result = result && validateReply(comment);
+            result = result && validateContent(comment.Content);
+        
+        
+            return result;
+        }
+
+        public static bool validateReply (Comment comment)
+        {
+            return comment.level > 0 && comment.repliedTo != null;
+        }
+
+        public static bool validateContent (string content)
+        {
+            return content.Length > 3 && content.Length <= 2000;
+        }
+        
+    }
+}
