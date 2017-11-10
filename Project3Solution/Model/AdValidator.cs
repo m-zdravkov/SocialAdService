ce81ace56ab22d76fb9cd27ec54ce871211ee74c
rootyTooty <rootyTooty@fartsan>
2017-11-10 10:22:47 +0100

    Added the AdValidator class.

---
+++ Project3Solution/Model/AdValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Model
+{
+    class AdValidator
+    {
+        //TO DO: Figure out if there is any attributes to validate
+        //Any data inputted by the user that needs checked?
+    }
+}
