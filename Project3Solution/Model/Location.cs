c1bb97a416dca01d565dad2a6fdab5ff15ab85ce
rootyTooty <rootyTooty@fartsan>
2017-11-10 10:38:13 +0100

    Added the Location class, and LocationValidator. Check comments in Location class.

---
+++ Project3Solution/Model/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Location
+    {   //TODO: Figure out if we need the "type" attribute, or how else to implement it.
+        public string name { get; set; }
+    }
+}
---
+++ Project3Solution/Model/LocationValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace Model
+{
+    public static class LocationValidator
+    {
+        public static Regex _nameRegex = new Regex(@"[a-zA-Z]{4,128}");
+
+        public static Location locationObj { get; set; }
+
+        public static bool ValidateLocation (this Location location)
+        {
+            bool result = true;
+
+            result = result && ValidateName(location.name);
+
+            return result;
+        }
+
+        public static bool ValidateName (string name)
+        {
+            return _nameRegex.Match(name).Success;
+        }
+    }
+}
