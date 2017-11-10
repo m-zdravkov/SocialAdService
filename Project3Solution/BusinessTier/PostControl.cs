74d13bb8b015e0eadafadfc60f518508440d075a
rootyTooty <rootyTooty@fartsan>
2017-11-10 10:18:20 +0100

    Added the singleton: PostControl.

---
+++ Project3Solution/BusinessTier/PostControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace BusinessTier
+{
+    class PostControl
+    {
+        private static PostControl _instance;
+        private PostControl ()
+        {
+
+        }
+
+        public static PostControl GetInstance ()
+        {
+            if (_instance == null)
+            {
+                _instance = new PostControl();
+            }
+            return _instance;
+        }
+
+        public void AddPost (Post post)
+        {
+
+        }
+
+        
+    }
+}
