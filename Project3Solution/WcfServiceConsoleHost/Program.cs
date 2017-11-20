6d476accd92e7e529330e4e72e7431c0adeaf972
Mihail Zdravkov Zdravkov <1061991@ucn.dk>
2017-11-20 09:51:21 +0100

    ServiceHost

---
+++ Project3Solution/WcfServiceConsoleHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceTier;
+
+namespace WcfServiceConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(SocialAdService)))
+            {
+                host.Open();
+                Console.WriteLine("Social Ad service is hosted...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
