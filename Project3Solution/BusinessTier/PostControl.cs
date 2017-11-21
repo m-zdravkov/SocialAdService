a1aafb0f1cdfa5d6061e93360ad05826b7250645
DeniZhekova <denitsa.zhrkova97@gmail.com>
2017-11-21 23:20:31 +0100

    Review(Model + Controller)

--- Project3Solution/BusinessTier/PostControl.cs
+++ Project3Solution/BusinessTier/PostControl.cs
@@ -10,6 +10,12 @@ namespace BusinessTier
     class PostControl
     {
         private static PostControl _instance;
+        User user = new User();
+        Post post = new Post();
+        //this.syncObject = new object ();
+        //this._reviews = new List<Review>();
+        //this._cumulativeReviewScore = 0;
+        //this._cumulativeReviewCount = 0
         private PostControl ()
         {
 
@@ -67,6 +73,38 @@ namespace BusinessTier
             return pagedQuery;
         }
 
+        public ReviewResult Reviews
+        {
+            get
+            {
+                return new ReviewResult(this._averageRatingScore, this._reviews);
+            }
+        }
 
+        public void AddReview(Review review)
+        {
+            lock (this.syncObject)
+            {
+                if (this._reviews.Any(r => user.Id == user.Id))
+                {
+                    throw new ReviewValidator(
+                        string.Format("User with Id {0} has already submitted a review for post with Id {1}", user.Id, post.Id));
+                }
+
+                this._reviews.Add(review);
+                this._cumulativeReviewScore += review.RatingScore;
+                this._cumulativeReviewCount++;
+                this._averageRatingScore = Math.Round((double)(this._cumulativeReviewScore / this._cumulativeReviewCount), 1);
+            }
+        }
+        private object syncObject;
+
+        private List<Review> _reviews;
+
+        private ulong _cumulativeReviewScore;
+
+        private uint _cumulativeReviewCount;
+
+        private double _averageRatingScore;
     }
 }
---
+++ Project3Solution/Model/ReviewResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public sealed class ReviewResult
+    {
+        private double _averageRatingScore;
+        private List<Review> _reviews;
+
+        public double AverageReviewScore { get; private set; }
+
+        public IEnumerable<Review> Reviews { get; private set; }
+
+        internal ReviewResult(double averageScore, IEnumerable<Review> reviews)
+        {
+            this.AverageReviewScore = averageScore;
+            this.Reviews = reviews;
+        }
+
+        public ReviewResult(double averageRatingScore, List<Review> reviews)
+        {
+            _averageRatingScore = averageRatingScore;
+            _reviews = reviews;
+        }
+    }
+
+}
